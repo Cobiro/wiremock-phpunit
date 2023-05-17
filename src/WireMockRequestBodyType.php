549cc2660872335b058c2939f63f9c4f68a56454
wslawski <wojciech.slawski@lifeworks.com>
2023-05-17 18:02:25 +0200

    Allow validating XML, validate JSON properly

---
+++ src/WireMockRequestBodyType.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WireMock\Phpunit;
+
+final class WireMockRequestBodyType
+{
+    public const JSON = 'json';
+    public const XML = 'xml';
+    public const RAW = 'raw';
+}
